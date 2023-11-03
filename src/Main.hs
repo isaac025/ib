3e2fca4e3dd5d67b974285c21703e9c505da642e
Isaac Lopez <isaac.lopez@upr.edu>
2023-11-03 12:31:04 -0400

    added nickname and channel

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,33 @@
 module Main where
 
-import System.IO
 import Network.Socket qualified as N
+import System.IO
 
+-- configuration options
+myServer :: String
+myServer = "irc.freenode.org"
+
+myPort :: N.PortNumber
+myPort = 6667
+
+myChan :: String
+myChan = "#tutbot-testing"
+
+myNick :: String
+myNick = "tutbot"
+
+-- top-level program
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    h <- connectTo myServer myPort
+    t <- hGetContents h
+    hSetBuffering stdout NoBuffering
+    print t
+
+-- connect to server given its name and port number
+connectTo :: N.HostName -> N.PortNumber -> IO Handle
+connectTo host port = do
+    (addr : _) <- N.getAddrInfo Nothing (Just host) (Just (show port))
+    sock <- N.socket (N.addrFamily addr) (N.addrSocketType addr) (N.addrProtocol addr)
+    N.connect sock (N.addrAddress addr)
+    N.socketToHandle sock ReadWriteMode
